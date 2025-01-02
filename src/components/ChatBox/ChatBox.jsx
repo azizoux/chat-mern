e9dfad606f197d37ab7c4714325fb0024d98edc1
Abdelaziz Mahamat Ali <azizmahamat@gmail.com>
2025-01-02 11:07:29 +0100

    ui for chat box builded

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,55 @@
 import React from "react";
 import "./ChatBox.css";
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
   return (
-    <div>
-      <h1>Chat Box</h1>
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          Abdelaziz Mahamat{" "}
+          <img src={assets.green_dot} alt="" className="dot" />
+        </p>
+        <img src={assets.help_icon} className="help" alt="" />
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum is placeholder text commomly used in ...
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className="msg-img" src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum is placeholder text commomly used in ...
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:35PM</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-input">
+        <input type="text" placeholder="Send a message" />
+        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
   );
 };
